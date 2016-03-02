de2034d78e495ed4687907f1dfc248cacce9975e
Subsentient <thinkingrodent@gmail.com>
2016-03-02 02:22:15 -0700

    Disable libstdc++, RTTI, and exceptions.

--- src/nexus.h
+++ src/nexus.h
@@ -6,6 +6,7 @@
 #define __NEXUS_HEADER__
 #define Exit(Code) printf("Exit code %s at %s:%d\n", #Code, __FILE__, __LINE__); exit(Code)
 #include <stdbool.h>
+#include <stddef.h>
 #include "server.h"
 
 #define NEXUS_VERSION "0.1"
@@ -16,4 +17,27 @@ void NEXUS_IRC2NEXUS(const char *Message);
 void NEXUS_NEXUS2IRC(const char *Message, struct ClientList *const Client);
 void NEXUS_DescriptorSet_Add(const int Descriptor);
 bool NEXUS_DescriptorSet_Del(const int Descriptor);
+
+//We need operator new and operator delete
+inline void *operator new(size_t Size)
+{
+	void *malloc(size_t);
+	return malloc(Size);
+}
+
+inline void *operator new[](size_t Size)
+{
+	return operator new(Size);
+}
+
+inline void operator delete(void *Ptr)
+{
+	void free(void*);
+	free(Ptr);
+}
+
+inline void operator delete[](void *Ptr)
+{
+	operator delete(Ptr);
+}
 #endif //__NEXUS_HEADER__
